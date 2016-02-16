2afd479aa9a31f27b7882b98728f2bae8ff40c33
tiancui <tcstory@163.com>
2016-02-16 20:27:20 +0800

    修改用户登录的接口

--- src/components/login-window.vue
+++ src/components/login-window.vue
@@ -203,7 +203,7 @@
             signIn: function () {
                 var _myself = this;
                 if (Utils.validate(_myself.userName)) {
-                    _myself.$http.post(ConfigMap.apiServer + '/sign-in', {
+                    _myself.$http.post(ConfigMap.apiServer + '/serv/user/sign-in', {
                         user_name: _myself.userName,
                         user_password: _myself.userPassword
                     }).then(function (response) {
