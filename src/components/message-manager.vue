43cb5f6f645f84c27966eb5bc3a5077c50903f46
tiancui <tcstory@163.com>
2016-02-26 22:23:59 +0800

    写了一个简单的消息管理器

--- src/components/message-manager.vue
+++ src/components/message-manager.vue
@@ -1,40 +1,63 @@
 <template>
-    <div class="message-window-wrapper" v-show="messages.length!==0">
-        <div v-for="message in messages" class="message-window" v-bind:class="message.type">
-            <div class="close-window-btn" v-on:click.stop="removeMessage"><i class="fa fa-times "></i></div>
-            <header class="message-title" v-text="message.title"></header>
-            <p class="message-content" v-text="message.content"></p>
-        </div>
+    <div class="message-window" v-bind:class="message.type" v-show="showMsg" transition="showingmsg">
+        <div class="close-window-btn" v-on:click.stop="removeMessage"><i class="fa fa-times "></i></div>
+        <header class="message-title" v-text="message.title"></header>
+        <p class="message-content" v-text="message.content"></p>
     </div>
 </template>
 
 <style scoped>
-    .message-window-wrapper {
-        position: fixed;
-        right: 1rem;
-        top: 0;
-        z-index: 500;
-    }
     .message-window {
         width: 15rem;
         min-height: 4rem;
         border-radius: .5rem;
         padding: 1em 1.5em;
-        -webkit-transition: 450ms all;
-        transition: 405ms all;
-        visibility: hidden;
-        opacity: 0;
-        margin-top: 1rem;
+        position: fixed;
+        right: 1rem;
+        z-index: 200;
     }
+
     .message-window.error {
         box-shadow: 0 0 0 1px hsl(0, 42%, 79%) inset, 0 0 0 0 hsla(0, 0%, 0%, 0);
         color: hsl(1, 48%, 42%);
         background-color: hsl(0, 100%, 98%);
-        /*-webkit-transform: translateY(1rem);*/
-        /*transform: translateY(1rem);*/
-        visibility: visible;
-        opacity: 1;
     }
+
+    .showingmsg-enter {
+        animation: 450ms showingmsg-in;
+    }
+    .showingmsg-leave {
+        animation: 450ms showingmsg-out;
+    }
+    .showingmsg-transition {
+        transform: translateY(1rem);
+    }
+    @keyframes showingmsg-in {
+        0% {
+            transform: translateY(0);
+            visibility: hidden;
+            opacity: 0;
+        }
+        100% {
+            transform: translateY(1rem);
+            visibility: visible;
+            opacity: 1;
+        }
+    }
+
+    @keyframes showingmsg-out {
+        from {
+            transform: translateY(1rem);
+            visibility: visible;
+            opacity: 1;
+        }
+        to {
+            transform: translateY(0);
+            visibility: hidden;
+            opacity: 0;
+        }
+    }
+
     .message-title {
         font-size: 1rem;
         font-weight: bold;
@@ -51,12 +74,6 @@
         cursor: pointer;
     }
 
-    .message-window.open {
-        -webkit-transform: translateY(1rem);
-        transform: translateY(1rem);
-        visibility: visible;
-        opacity: 1;
-    }
 </style>
 
 <script>
@@ -67,23 +84,32 @@
     module.exports = {
         data: function () {
             return {
-                messages: []
-            }
-        },
-        watch: {
-            messages: function () {
-                if(this.messages.length > configMap.maxMessageNumber) {
-                    this.messages.shift();
-                }
+                message: {
+                    title: '',
+                    content: '',
+                    type: ''
+                },
+                showMsg: false
             }
         },
         methods: {
             removeMessage: function () {
+//                this.message.title = '';
+//                this.message.content = '';
+//                this.message.type = '';
+                this.showMsg = false;
             }
         },
         events: {
             'showmessagewindow': function (msg) {
-                this.messages.unshift(msg);
+                var _myself = this;
+                _myself.showMsg = true;
+                _myself.message.type = msg.type;
+                _myself.message.title = msg.title;
+                _myself.message.content = msg.content;
+                setTimeout(function () {
+                    _myself.showMsg = false;
+                }, 1500);
             }
         }
     }
